81edef92c3d936ac8c2eba81a126c30357857321
Elena Santana <fanelenan7@gmail.com>
2018-03-23 14:25:45 -0400

    filtering by type of building works

--- src/Test.js
+++ src/Test.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
+
 import RemineTable from './components/Table/RemineTable/RemineTable';
-import Filters from './Filters';
 import api from './API';
+import BuildingTypes from './components/BuildingTypes';
 
 class Test extends Component {
   constructor() {
     super();
     this.state = {
-      locations: []
+      locations: [],
+      buildingTypes: [],
+      typeFilter: [],
+      MinBedsFilter: 0,
+      MaxBedsFilter: 10,
+      MinBathsFilter: 0,
+      MaxBathsFilter: 10,
+      filteredData: []
     }
   }
 
   componentWillMount() {
+    this.getLocations();
+    this.getBuildingTypes();
+  }
+
+  getLocations() {
     api.getLocations().then((response) => {
       this.setState({
         locations: response.data
@@ -19,13 +32,64 @@ class Test extends Component {
     })
   }
 
+  getBuildingTypes() {
+    api.getBuildingTypes().then((response) => {
+      response.data.map((type) => {
+        type.displayName = type.name.charAt(0).toUpperCase() + type.name.slice(1);
+        type.displayName = type.displayName.replace(/([A-Z])/g, ' $1').trim();
+      })
+      this.setState({
+        buildingTypes: response.data
+      })
+    })
+  }
+
+  handleTypeSelect(e) {
+    if (!this.state.typeFilter.includes(e.target.value)) {
+      this.setState({
+        typeFilter: [...this.state.typeFilter, e.target.value]
+      }, () => this.updateLocation())
+    } else {
+      const newFilter = this.state.typeFilter.filter((type) => {
+        return type !== e.target.value
+      })
+      this.setState({
+        typeFilter: newFilter,
+      }, () => this.updateLocation())
+    }
+  }
+
+  // on any change to filters, check what changed and rerender with new parameters
+  // save in it's own variable to protect the original data from mutating in case
+  // we need to deselect.
+  updateLocation() {
+    const newLocations = this.state.locations.filter(location => {
+      return this.state.typeFilter.includes(location.buildingType.name)
+    })
+    this.setState({
+      filteredData: newLocations
+    })
+  }
+
   render() {
     return (
       <div className="testContainer">
-        <div className="filterContainer">
-          <Filters />
-        </div>
-        <RemineTable properties={this.state.locations} />
+        <section className="filters">
+          <h3>Select Building Type</h3>
+          <BuildingTypes
+            buildingTypes={this.state.buildingTypes}
+            onTypeSelect={e => this.handleTypeSelect(e)}
+          />
+          <div className="filters__ranges">
+            <h3>Select Number of Bedrooms</h3>
+            <h3>Select Number of Bathrooms</h3>
+          </div>
+        </section>
+        { this.state.filteredData.length > 0 ?
+          (<RemineTable properties={this.state.filteredData} />) :
+          (<RemineTable properties={this.state.locations} />)
+        }
+
       </div>
     );
   }
--- src/components/BuildingTypes.js
+++ src/components/BuildingTypes.js
@@ -4,12 +4,11 @@ export default class BuildingTypes extends Component {
   render() {
     const buildingTypes = this.props.buildingTypes.map((buildtype, key) => {
       return (
-        <label>
+        <label key={key}>
           <input
             type="checkbox"
             className="buildtypes__button"
             onClick={e => this.props.onTypeSelect(e)}
-            key={key}
             value={buildtype.name}
           />
           {buildtype.displayName}
